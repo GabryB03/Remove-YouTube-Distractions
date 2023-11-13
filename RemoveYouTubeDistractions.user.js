b575acb5daa37450798957d2adf190cf4b753d41
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-11-13 04:35:05 +0100

    Update RemoveYouTubeDistractions.user.js

--- RemoveYouTubeDistractions.user.js
+++ RemoveYouTubeDistractions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Remove Distractions
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Remove all the YouTube distractions to get concentrated on what's really important
 // @author       GabryB03
 // @match        https://www.youtube.com/*
@@ -11,73 +11,17 @@
 // @grant        none
 // ==/UserScript==
 
+
 (function()
 {
     'use strict';
-
-    function asyncLoop()
-    {
-        try
-        {
-            var distractingElements = ["/feed/library", "/feed/history", "/playlist?list=WL", "/playlist?list=LL", "/account", "/reporthistory"];
-
-            for (var i = 0; i < distractingElements.length; i++)
-            {
-                var distractingElement = document.querySelector("a[href='" + distractingElements[i] + "']");
-
-                if (distractingElement != null && distractingElement != undefined)
-                {
-                    distractingElement.remove();
-                }
-            }
-
-            distractingElements = ["Guida", "Invia feedback"];
-
-            for (var i = 0; i < distractingElements.length; i++)
-            {
-                var distractingElement = document.querySelector("a[title='" + distractingElements[i] + "']");
-
-                if (distractingElement != null && distractingElement != undefined)
-                {
-                    distractingElement.remove();
-                }
-            }
-
-            distractingElements = ["contentContainer", "buttons", "start"];
-
-            for (var i = 0; i < distractingElements.length; i++)
-            {
-                var distractingElement = document.getElementById(distractingElements[i]);
-
-                if (distractingElement != null && distractingElement != undefined)
-                {
-                    distractingElement.remove();
-                }
-            }
-
-            distractingElements = ["Esegui una ricerca vocale", "Notifiche"];
-
-            for (var i = 0; i < distractingElements.length; i++)
-            {
-                var distractingElement = document.querySelector("button[aria-label='" + distractingElements[i] + "']");
-
-                if (distractingElement != null && distractingElement != undefined)
-                {
-                    distractingElement.remove();
-                }
-            }
-        }
-        catch (e)
-        {
-
-        }
-
-        setTimeout(async function()
-        {
-            await asyncLoop();
-        },
-        500);
-    }
-
-    asyncLoop();
+    const css =
+    `
+        #contentContainer, #buttons, #start, a[href='/feed/library'], a[href='/feed/history'], a[href='/playlist?list=WL'], a[href='/playlist?list=LL'], a[href='/account'], a[href='/reporthistory'], a[title='Guida'], a[title='Invia feedback'], button[aria-label='Esegui una ricerca vocale'], button[aria-label='Notifiche'] { display: none !important; visibility: hidden !important; }
+    `;
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const styleElement = document.createElement('style');
+    styleElement.type = 'text/css';
+    styleElement.innerHTML = css;
+    head.appendChild(styleElement);
 })();
