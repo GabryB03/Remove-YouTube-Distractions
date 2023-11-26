35df5eb1a6fd9c3977b07509908c9a1afbff9478
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-11-26 16:37:28 +0100

    Update RemoveYouTubeDistractions.user.js

--- RemoveYouTubeDistractions.user.js
+++ RemoveYouTubeDistractions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Remove Distractions
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Remove all the YouTube distractions to get concentrated on what's really important
 // @author       GabryB03
 // @match        https://www.youtube.com/*
@@ -16,6 +16,6 @@
     const head = document.head || document.getElementsByTagName('head')[0];
     const styleElement = document.createElement('style');
     styleElement.type = 'text/css';
-    styleElement.innerHTML = "#contentContainer,#buttons,#start,a[href='/feed/library'],a[href='/feed/history'],a[href='/playlist?list=WL'],a[href='/playlist?list=LL'],a[href='/account'],a[href='/reporthistory'],a[title='Guida'],a[title='Invia feedback'],button[aria-label='Esegui una ricerca vocale'],button[aria-label='Notifiche']{display:none!important;visibility:hidden!important;}";
+    styleElement.innerHTML = "#contentContainer,#buttons,#start,a[href='/feed/library'],a[href='/feed/history'],a[href='/playlist?list=WL'],a[href='/playlist?list=LL'],a[href='/account'],a[href='/reporthistory'],a[title='Guida'],a[title='Invia feedback'],button[aria-label='Esegui una ricerca vocale'],button[aria-label='Notifiche'],.ytd-reel-shelf-renderer{display:none!important;visibility:hidden!important;}";
     head.appendChild(styleElement);
 })();
